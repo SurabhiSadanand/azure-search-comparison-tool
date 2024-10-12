752473dd28d30fd5df388f7cdb7f25c9fa326244
Surabhi <surabhisadanand92@gmail.com>
2024-10-13 02:37:28 +0530

    Update app.py

--- app/backend/app.py
+++ app/backend/app.py
@@ -244,3 +244,7 @@ def create_app():
     app = Quart(__name__)
     app.register_blueprint(bp)
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=8000)
